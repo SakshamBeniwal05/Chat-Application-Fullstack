041d1eba2c9ce37cebe1f0d7ffee9cc1c5dc35d1
SakshamBeniwal05 <sakshambeniwal123@gmail.com>
2026-01-28 22:00:08 +0530

    ....

--- Frontend/src/components/Nav/NavBar.tsx
+++ Frontend/src/components/Nav/NavBar.tsx
@@ -18,7 +18,7 @@ const NavBar = () => {
         </div>
         <div className="flex items-center gap-3">
           <Link to={`/settings`}>
-            <button type="button" className="btn btn-sm bg-secondary text-secondary-content hover:bg-secondary/80 rounded-full">
+            <button type="button" className="btn btn-sm bg-secondary text-secondary-content hover:bg-secondary/80 rounded-full  size-12">
               <Settings size={18} />
             </button>
           </Link>
@@ -27,9 +27,9 @@ const NavBar = () => {
             <button
               type="button"
               onClick={logout}
-              className="btn btn-sm bg-accent text-accent-content hover:bg-accent/80 rounded-full"
+              className="btn btn-sm bg-accent text-accent-content hover:bg-accent/80 rounded-full size-12"
             >
-              <LogOut size={18} />
+              <LogOut />
             </button>
           )}
         </div>
--- Frontend/src/pages/HomePage/HomePage.tsx
+++ Frontend/src/pages/HomePage/HomePage.tsx
@@ -142,7 +142,7 @@ const HomePage = () => {
                         {/* ================= Chat Area ================= */}
                         {!currentReciver ? (
                             // No Chat Selected
-                            <div className="w-full flex flex-1 flex-col items-center justify-center p-16 bg-base-100/50 overflow-y-scroll">
+                            <div className="w-full flex flex-1 flex-col items-center justify-center p-16 bg-base-100/50 overflow-y-scroll overflow-x-hidden">
                                 <div className="max-w-md text-center space-y-6">
                                     <div className="flex justify-center gap-4 mb-4">
                                         <div className="relative">
@@ -151,7 +151,7 @@ const HomePage = () => {
                                             </div>
                                         </div>
                                     </div>
-                                    <h2 className="text-2xl font-bold">Welcome to Chatty!</h2>
+                                    <h2 className="text-2xl font-bold">Welcome to LUFFY!</h2>
                                     <p className="text-base-content/60">
                                         Select a conversation from the sidebar to start chatting
                                     </p>
