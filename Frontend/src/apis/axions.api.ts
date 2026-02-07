becae1fd99eb45aa4ded3292ee3ebbafda9551d5
SakshamBeniwal05 <sakshambeniwal123@gmail.com>
2026-02-07 17:52:13 +0530

    fsdfsfds

--- Frontend/src/apis/axions.api.ts
+++ Frontend/src/apis/axions.api.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.NODE_ENV ? "http://localhost:8000/api" : "/api",
+    baseURL: import.meta.env.NODE_ENV ? "http://localhost:8000/api" : "https://chat-application-fullstack-pgs6.onrender.com/api",
     withCredentials: true
 })
