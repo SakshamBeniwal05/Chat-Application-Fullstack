a6490ad9c49af7f08857f76fddb10ea946504351
SakshamBeniwal05 <sakshambeniwal123@gmail.com>
2026-02-07 17:37:09 +0530

    ...

--- Frontend/src/apis/axions.api.ts
+++ Frontend/src/apis/axions.api.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL: process.env.NODE_ENV ? "http://localhost:8000/api" : "/api",
+    baseURL: import.meta.env.NODE_ENV ? "http://localhost:8000/api" : "/api",
     withCredentials: true
 })
